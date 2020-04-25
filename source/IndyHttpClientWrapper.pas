8f26a502e2775831daaf97f4b56a752038c80180
Dennis Glittenberg <glittenberg.d@gmail.com>
2020-04-25 12:23:04 +0200

    Anpassungen am Client-Wrapper

--- source/IndyHttpClientWrapper.pas
+++ source/IndyHttpClientWrapper.pas
@@ -3,15 +3,49 @@ unit IndyHttpClientWrapper;
 interface
 
 uses
-  HttpClientWrapperLib, IdHTTP, System.Classes;
+  HttpClientWrapperLib, IdHTTP, System.Classes, IdSSLOpenSSL, System.SysUtils;
 
 type
-  TIndyHttpClientWrapper = class(TInterfacedObject, IHttpClientWrapper)
+  TIndyClientResponse = class(TInterfacedObject, IHTTPClientResponse)
+  private
+    FStatusCode: Integer;
+    FContentStream: TMemoryStream;
+    FContent: String;
+    function getStatusCode: Integer;
+    function getContent: String;
+    function getContentStream: TStream;
   public
+    constructor Create;
+    destructor Destroy;override;
+    procedure LoadContentStream(stream: TStream);
+    procedure SaveContentStream(filename: String);
+
+    property StatusCode: Integer read getStatusCode write FStatusCode;
+    property Content: String read getContent;
+    property ContentStream: TStream read getContentStream;
+  end;
+
+  TIndyHttpClientWrapper = class(TInterfacedObject, IHttpClientWrapper, IHttpClientFileDownload)
+  private
+    FBasicAuthUsername: String;
+    FBasicAuthPassword: String;
+    procedure InitSSL(sslIOHandler: TIdSSLIOHandlerSocketOpenSSL);
+    procedure InitHttp(http: TIdHTTP);
+  public
+    procedure SetBasicAuth(username, password: String);
     function Get(url: String): IHTTPClientResponse;overload;
     function Get(url: String; out response: String): IHTTPClientResponse;overload;
+    function Get(url: String; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
 
-    function Post(url: String; ContentType: String; postData: TStream): IHTTPClientResponse;
+    function DownloadFile(url: string; filename: String): Boolean;
+
+    function Post(url: String; ContentType: String; postData: TStream): IHTTPClientResponse;overload;
+    function Post(url: String; ContentType: String; postData: TStream; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
+    function Post(url: String; ContentType: String; postData: TStrings; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
+
+    function Put(url: String; ContentType: String; postData: TStream; header: TArray<THttpHeader>): IHTTPClientResponse;
+
+    function Delete(url: String; header: TArray<THttpHeader>): IHTTPClientResponse;
   end;
 
 implementation
@@ -26,44 +60,57 @@ var
   respStream: TMemoryStream;
   respText: string;
   http: TIdHTTP;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
 begin
   http := TIdHTTP.Create;
-  Result := THTTPClientResponse.Create;
+
+  Result := TIndyClientResponse.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
   respStream := TMemoryStream.Create;
   try
     http.Get(url, respStream);
     respStream.Position := 0;
     respText := ReadStringAsCharset(respStream, http.Response.CharSet);
-    THTTPClientResponse(Result).Content := respText;
-    THTTPClientResponse(Result).LoadContentStream(respStream);
-    THTTPClientResponse(Result).StatusCode := http.ResponseCode;
+    TIndyClientResponse(Result).FContent := respText;
+    TIndyClientResponse(Result).LoadContentStream(respStream);
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
   except
     on E: EIdHTTPProtocolException do
     begin
-      THTTPClientResponse(Result).StatusCode := E.ErrorCode;
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
     end;
   end;
   http.Free;
+  ioHandler.Free;
   respStream.Free;
 end;
 
 function TIndyHttpClientWrapper.Get(url: String; out response: String): IHTTPClientResponse;
 var
   http: TIdHTTP;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
 begin
   http := TIdHTTP.Create;
-  Result := THTTPClientResponse.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+  Result := TIndyClientResponse.Create;
   try
     response := http.Get(url);
-    THTTPClientResponse(Result).Content := response;
-    THTTPClientResponse(Result).StatusCode := http.ResponseCode;
+    TIndyClientResponse(Result).FContent := response;
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
   except
     on E: EIdHTTPProtocolException do
     begin
-      THTTPClientResponse(Result).StatusCode := E.ErrorCode;
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
     end;
   end;
   http.Free;
+  ioHandler.Free;
 end;
 
 function TIndyHttpClientWrapper.Post(url: String;
@@ -71,22 +118,303 @@ function TIndyHttpClientWrapper.Post(url: String;
 var
   http: TIdHTTP;
   resp: string;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
 begin
-  Result := THTTPClientResponse.Create;
+  Result := TIndyClientResponse.Create;
   http := TIdHTTP.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
   http.Request.UserAgent := ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
   try
     http.Request.ContentType := ContentType;
 
     resp := http.Post(url, postData);
-    THTTPClientResponse(Result).Content := resp;
+    TIndyClientResponse(Result).FStatusCode := http.ResponseCode;
+    TIndyClientResponse(Result).FContent := resp;
   except
     on E: EIdHTTPProtocolException do
     begin
-      THTTPClientResponse(Result).StatusCode := E.ErrorCode;
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+      TIndyClientResponse(Result).FContent := E.ErrorMessage;
     end;
   end;
   http.Free;
+  ioHandler.Free;
+end;
+
+function TIndyHttpClientWrapper.Delete(url: String; header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TIdHTTP;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+  h: THttpHeader;
+begin
+  Result := TIndyClientResponse.Create;
+  http := TIdHTTP.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+  try
+    http.Request.UserAgent := ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+    for h in header do
+    begin
+      http.Request.CustomHeaders.AddValue(h.key, h.value);
+    end;
+
+    try
+      http.Delete(url);
+      TIndyClientResponse(Result).StatusCode := http.ResponseCode;
+    except
+      on E: EIdHTTPProtocolException do
+      begin
+        TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+      end;
+    end;
+  finally
+    http.Free;
+    ioHandler.Free;
+  end;
+end;
+
+function TIndyHttpClientWrapper.DownloadFile(url: string; filename: String): Boolean;
+var
+  fs: TFileStream;
+  http: TIdHTTP;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+begin
+  Result := false;
+  http := TIdHTTP.Create;
+
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+
+  fs := TFileStream.Create(filename, fmCreate);
+  try
+    http.Get(url, fs);
+    Result := true;
+  finally
+    fs.Free;
+    http.Free;
+    ioHandler.Free;
+  end;
+end;
+
+function TIndyHttpClientWrapper.Get(url: String;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  respStream: TMemoryStream;
+  respText: string;
+  http: TIdHTTP;
+  h: THttpHeader;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+begin
+  http := TIdHTTP.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+  Result := TIndyClientResponse.Create;
+  respStream := TMemoryStream.Create;
+  try
+    for h in header do
+    begin
+      http.Request.CustomHeaders.AddValue(h.key, h.value);
+    end;
+    http.Get(url, respStream);
+    respStream.Position := 0;
+    respText := ReadStringAsCharset(respStream, http.Response.CharSet);
+    TIndyClientResponse(Result).FContent := respText;
+    TIndyClientResponse(Result).LoadContentStream(respStream);
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
+  except
+    on E: EIdHTTPProtocolException do
+    begin
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+    end;
+  end;
+  http.Free;
+  ioHandler.Free;
+  respStream.Free;
+end;
+
+procedure TIndyHttpClientWrapper.InitHttp(http: TIdHTTP);
+begin
+  http.Request.Username := FBasicAuthUsername;
+  http.Request.Password := FBasicAuthPassword;
+  http.Request.BasicAuthentication := not FBasicAuthUsername.Trim.IsEmpty;
+end;
+
+procedure TIndyHttpClientWrapper.InitSSL(
+  sslIOHandler: TIdSSLIOHandlerSocketOpenSSL);
+begin
+  sslIOHandler.SSLOptions.Method := sslvTLSv1_2;
+end;
+
+function TIndyHttpClientWrapper.Post(url, ContentType: String;
+  postData: TStrings; header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TIdHTTP;
+  h: THttpHeader;
+  response: TMemoryStream;
+  respText: string;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+begin
+  http := TIdHTTP.Create;
+  Result := TIndyClientResponse.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+
+  http.Request.UserAgent := ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+  response := TMemoryStream.Create;
+  try
+    http.Request.ContentType := ContentType;
+    for h in header do
+    begin
+      http.Request.CustomHeaders.AddValue(h.key, h.value);
+    end;
+
+    http.Post(url, postData, response);
+
+    response.Position := 0;
+    respText := ReadStringAsCharset(response, http.Response.CharSet);
+    TIndyClientResponse(Result).FContent := respText;
+    TIndyClientResponse(Result).LoadContentStream(response);
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
+  except
+    on E: EIdHTTPProtocolException do
+    begin
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+    end;
+  end;
+  http.Free;
+  response.Free;
+  ioHandler.Free;
+end;
+
+function TIndyHttpClientWrapper.Put(url, ContentType: String; postData: TStream;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TIdHTTP;
+  resp: string;
+  h: THttpHeader;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+begin
+  Result := TIndyClientResponse.Create;
+  http := TIdHTTP.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+  http.Request.UserAgent := ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+  try
+    http.Request.ContentType := ContentType;
+    for h in header do
+    begin
+      http.Request.CustomHeaders.AddValue(h.key, h.value);
+    end;
+
+    resp := http.Put(url, postData);
+    TIndyClientResponse(Result).FContent := resp;
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
+  except
+    on E: EIdHTTPProtocolException do
+    begin
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+    end;
+  end;
+  http.Free;
+  ioHandler.Free;
+end;
+
+procedure TIndyHttpClientWrapper.SetBasicAuth(username, password: String);
+begin
+  FBasicAuthUsername := username;
+  FBasicAuthPassword := password;
+end;
+
+function TIndyHttpClientWrapper.Post(url, ContentType: String;
+  postData: TStream; header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TIdHTTP;
+  resp: string;
+  h: THttpHeader;
+  ioHandler: TIdSSLIOHandlerSocketOpenSSL;
+begin
+  Result := TIndyClientResponse.Create;
+  http := TIdHTTP.Create;
+  ioHandler := TIdSSLIOHandlerSocketOpenSSL.Create;
+  http.IOHandler := ioHandler;
+  InitSSL(ioHandler);
+  InitHttp(http);
+  http.Request.UserAgent := ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+  try
+    http.Request.ContentType := ContentType;
+    for h in header do
+    begin
+      http.Request.CustomHeaders.AddValue(h.key, h.value);
+    end;
+
+    resp := http.Post(url, postData);
+    TIndyClientResponse(Result).FContent := resp;
+    TIndyClientResponse(Result).StatusCode := http.ResponseCode;
+  except
+    on E: EIdHTTPProtocolException do
+    begin
+      TIndyClientResponse(Result).FContent := E.ErrorMessage;
+      TIndyClientResponse(Result).StatusCode := E.ErrorCode;
+    end;
+  end;
+  http.Free;
+  ioHandler.Free;
+end;
+
+{ TIndyClientResponse }
+
+constructor TIndyClientResponse.Create;
+begin
+  FContentStream := TMemoryStream.Create;
+end;
+
+destructor TIndyClientResponse.Destroy;
+begin
+  FContentStream.Free;
+  inherited;
+end;
+
+function TIndyClientResponse.getContent: String;
+begin
+  Result := FContent;
+end;
+
+function TIndyClientResponse.getContentStream: TStream;
+begin
+  Result := FContentStream;
+end;
+
+
+function TIndyClientResponse.getStatusCode: Integer;
+begin
+  Result := FStatusCode;
+end;
+
+
+procedure TIndyClientResponse.LoadContentStream(stream: TStream);
+begin
+  stream.Position := 0;
+  FContentStream.LoadFromStream(stream);
+end;
+
+
+procedure TIndyClientResponse.SaveContentStream(filename: String);
+begin
+  FContentStream.Position := 0;
+  FContentStream.SaveToFile(filename);
 end;
 
 end.
--- source/NetHttpClientWrapper.pas
+++ source/NetHttpClientWrapper.pas
@@ -3,21 +3,46 @@ unit NetHttpClientWrapper;
 interface
 
 uses
-  HttpClientWrapperLib, System.Classes;
+  HttpClientWrapperLib, System.Classes, System.Net.URLClient,
+  System.Net.HttpClient, System.Net.HttpClientComponent;
 
 type
-  TNetHttpClientWrapper = class(TInterfacedObject, IHttpClientWrapper)
+  TNetHttpClientWrapper = class(TInterfacedObject, IHttpClientWrapper, IHttpClientFileDownload)
+  private
+    FBasicAuthUser: String;
+    FBasicAuthPassword: String;
+    FUseCustomCertifcateValidation: Boolean;
+    function getHttpClient: TNetHttpClient;
+    procedure ValidateServerCertificate(const Sender: TObject; const ARequest: TURLRequest; const Certificate: TCertificate; var Accepted: Boolean);
+    procedure AuthEvent(const Sender: TObject; AnAuthTarget: TAuthTargetType; const ARealm, AURL: string; var AUserName, APassword: string; var AbortAuth: Boolean;
+      var Persistence: TAuthPersistenceType);
   public
+    constructor Create;
+    procedure SetBasicAuth(username, password: String);
     function Get(url: String): IHTTPClientResponse;overload;
+    function Get(url: String; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
     function Get(url: String; out response: String): IHTTPClientResponse;overload;
 
-    function Post(url: String; ContentType: String; postData: TStream): IHTTPClientResponse;
+    function DownloadFile(url: string; filename: String): Boolean;
+
+    function Post(url: String; ContentType: String; postData: TStream): IHTTPClientResponse; overload;
+    function Post(url: String; ContentType: String; postData: TStream; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
+    function Post(url: String; ContentType: String; postData: TStrings; header: TArray<THttpHeader>): IHTTPClientResponse;overload;
+
+    function Put(url: String; ContentType: String; postData: TStream; header: TArray<THttpHeader>): IHTTPClientResponse;
+
+    function Delete(url: String; header: TArray<THttpHeader>): IHTTPClientResponse;
+
+    property BasicAuthUser: String read FBasicAuthUser write FBasicAuthUser;
+    property BasicAuthPassword: String read FBasicAuthPassword write FBasicAuthPassword;
+
+    property UseCustomCertifcateValidation: Boolean read FUseCustomCertifcateValidation write FUseCustomCertifcateValidation;
   end;
 
 implementation
 
 uses
-  System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent;
+  System.NetEncoding, System.SysUtils;
 
 { TIndyHttpClientWrapper }
 
@@ -26,13 +51,65 @@ var
   http: TNetHTTPClient;
   resp: IHTTPResponse;
 begin
-  http := TNetHTTPClient.Create(nil);
+  http := getHttpClient;
   Result := THTTPClientResponse.Create;
   resp := http.Get(url);
-  THTTPClientResponse(Result).Content := resp.ContentAsString;
   THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
   THTTPClientResponse(Result).StatusCode := resp.StatusCode;
   http.Free;
+end;
+
+procedure TNetHttpClientWrapper.AuthEvent(const Sender: TObject; AnAuthTarget: TAuthTargetType; const ARealm,
+  AURL: string; var AUserName, APassword: string; var AbortAuth: Boolean; var Persistence: TAuthPersistenceType);
+begin
+  if AnAuthTarget = TAuthTargetType.Server then
+  begin
+    AUserName := BasicAuthUser;
+    APassword := BasicAuthPassword;
+  end;
+end;
+
+constructor TNetHttpClientWrapper.Create;
+begin
+  FUseCustomCertifcateValidation := false;
+end;
+
+function TNetHttpClientWrapper.Delete(url: String;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  h: THttpHeader;
+begin
+  http := getHttpClient;
+
+  for h in header do
+  begin
+    http.CustomHeaders[h.key] := h.value;
+  end;
+
+  Result := THTTPClientResponse.Create;
+  resp := http.Delete(url);
+  THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
+  THTTPClientResponse(Result).StatusCode := resp.StatusCode;
+  http.Free;
+end;
+
+function TNetHttpClientWrapper.DownloadFile(url, filename: String): Boolean;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  fs: TFileStream;
+begin
+  http := getHttpClient;
+  fs := TFileStream.Create(filename, fmCreate);
+  try
+    resp := http.Get(url, fs);
+    Result := true;
+  finally
+    fs.Free;
+    http.Free;
+  end;
 end;
 
 function TNetHttpClientWrapper.Get(url: String; out response: String): IHTTPClientResponse;
@@ -40,13 +117,128 @@ var
   http: TNetHTTPClient;
   resp: IHTTPResponse;
 begin
-  http := TNetHTTPClient.Create(nil);
+  http := getHttpClient;
   Result := THTTPClientResponse.Create;
   resp := http.Get(url);
-  THTTPClientResponse(Result).Content := resp.ContentAsString;
   THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
   THTTPClientResponse(Result).StatusCode := resp.StatusCode;
   response := Result.Content;
+  http.Free;
+end;
+
+function TNetHttpClientWrapper.getHttpClient: TNetHttpClient;
+begin
+  Result := TNetHTTPClient.Create(nil);
+  if (FUseCustomCertifcateValidation) or ((TOSVersion.Platform = TOSVersion.TPlatform.pfAndroid) and (not TOSVersion.Check(5, 0))) then
+  begin
+    Result.OnValidateServerCertificate := ValidateServerCertificate;
+  end;
+  if (TOSVersion.Platform = TOSVersion.TPlatform.pfWindows) and (not TOSVersion.Check(6, 2)) then
+  begin
+    Result.SecureProtocols := [THTTPSecureProtocol.TLS11, THTTPSecureProtocol.TLS12];
+  end;
+
+  if not BasicAuthUser.Trim.IsEmpty then
+  begin
+    Result.OnAuthEvent := AuthEvent;
+  end;
+end;
+
+function TNetHttpClientWrapper.Post(url, ContentType: String;
+  postData: TStrings; header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  h: THttpHeader;
+begin
+  http := getHttpClient;
+
+  for h in header do
+  begin
+    http.CustomHeaders[h.key] := h.value;
+  end;
+
+  Result := THTTPClientResponse.Create;
+  http.ContentType := ContentType;
+  resp := http.Post(url, postData);
+  THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
+  THTTPClientResponse(Result).StatusCode := resp.StatusCode;
+  http.Free;
+end;
+
+function TNetHttpClientWrapper.Put(url, ContentType: String; postData: TStream;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  h: THttpHeader;
+begin
+  http := getHttpClient;
+
+  for h in header do
+  begin
+    http.CustomHeaders[h.key] := h.value;
+  end;
+
+  Result := THTTPClientResponse.Create;
+  http.ContentType := ContentType;
+  resp := http.Put(url, postData);
+  THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
+  THTTPClientResponse(Result).StatusCode := resp.StatusCode;
+  http.Free;
+end;
+
+procedure TNetHttpClientWrapper.SetBasicAuth(username, password: String);
+begin
+  BasicAuthUser := username;
+  BasicAuthPassword := password;
+end;
+
+procedure TNetHttpClientWrapper.ValidateServerCertificate(const Sender: TObject;
+  const ARequest: TURLRequest; const Certificate: TCertificate;
+  var Accepted: Boolean);
+begin
+  Accepted := true;
+end;
+
+function TNetHttpClientWrapper.Post(url, ContentType: String; postData: TStream;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  h: THttpHeader;
+begin
+  http := getHttpClient;
+
+  for h in header do
+  begin
+    http.CustomHeaders[h.key] := h.value;
+  end;
+
+  Result := THTTPClientResponse.Create;
+  http.ContentType := ContentType;
+  resp := http.Post(url, postData);
+  THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
+  THTTPClientResponse(Result).StatusCode := resp.StatusCode;
+  http.Free;
+end;
+
+function TNetHttpClientWrapper.Get(url: String;
+  header: TArray<THttpHeader>): IHTTPClientResponse;
+var
+  http: TNetHTTPClient;
+  resp: IHTTPResponse;
+  h: THttpHeader;
+begin
+  http := getHttpClient;
+  for h in header do
+  begin
+    if (h.key <> '') and (h.value <> '') then http.CustomHeaders[h.key] := h.value;
+  end;
+  Result := THTTPClientResponse.Create;
+  resp := http.Get(url);
+  THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
+  THTTPClientResponse(Result).StatusCode := resp.StatusCode;
   http.Free;
 end;
 
@@ -56,15 +248,13 @@ var
   http: TNetHTTPClient;
   resp: IHTTPResponse;
 begin
-  http := TNetHTTPClient.Create(nil);
+  http := getHttpClient;
   Result := THTTPClientResponse.Create;
   http.ContentType := ContentType;
   resp := http.Post(url, postData);
-  THTTPClientResponse(Result).Content := resp.ContentAsString;
   THTTPClientResponse(Result).LoadContentStream(resp.ContentStream);
   THTTPClientResponse(Result).StatusCode := resp.StatusCode;
   http.Free;
 end;
-
 
 end.
